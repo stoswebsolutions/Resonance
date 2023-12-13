f8e0c98185ca43b745751014707669282c4d6559
srikanthyaswada <srikanthyaswada.vita@gmail.com>
2023-12-13 18:17:59 +0530

    page links in the address bar removed

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  statusClass = 'not-active';
+  scrollToHome(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+    this.statusClass = 'active';
+  }
+  scrollToCourse(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+    this.statusClass = 'active';
+  }
+  scrollToResults(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+    this.statusClass = 'active';
+  }
+  scrollToAbout(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+    this.statusClass = 'active';
+  }
+  scrollToContact(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+    this.statusClass = 'active';
+  }
 
 }
